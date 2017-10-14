e05a952a7c01b7848e52beab6b57f7fdc4685fc2
kenelite <wangguoyu0916@gmail.com>
2017-10-14 14:26:12 +0800

    get vm list from esxi

--- 050getvmlist.py
+++ 050getvmlist.py
@@ -2,112 +2,119 @@
 # -*- coding: UTF-8 -*-
 
 
-from __future__ import print_function
+import atexit
+import argparse
+import getpass
+import sys
+import textwrap
+import time
 
-from pyVim.connect import SmartConnect, Disconnect
+from pyVim import connect
 from pyVmomi import vim
 
-import argparse
-import atexit
-import getpass
+
+import requests
+requests.packages.urllib3.disable_warnings()
+
 import ssl
 
-def GetArgs():
-   """
-   Supports the command-line arguments listed below.
-   """
-   parser = argparse.ArgumentParser(
-       description='Process args for retrieving all the Virtual Machines')
-   parser.add_argument('-s', '--host', required=True, action='store',
-                       help='Remote host to connect to')
-   parser.add_argument('-o', '--port', type=int, default=443, action='store',
-                       help='Port to connect on')
-   parser.add_argument('-u', '--user', required=True, action='store',
-                       help='User name to use when connecting to host')
-   parser.add_argument('-p', '--password', required=False, action='store',
-                       help='Password to use when connecting to host')
-   args = parser.parse_args()
-   return args
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    # because -h is reserved for 'help' we use -s for service
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSphere service to connect to')
+
+    # because we want -p for password, we use -o for port
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use when connecting to host')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use when connecting to host')
+    parser.add_argument('-e', '--esxi',
+                        required=True,
+                        action='store',
+                        help='Name of the ESXi to look for.')
+
+    args = parser.parse_args()
+
+    if not args.password:
+        args.password = getpass.getpass(
+            prompt='Enter password for host %s and user %s: ' %
+                   (args.host, args.user))
+
+    return args
 
 
-def PrintVmInfo(vm, depth=1):
-   """
-   Print information for a particular virtual machine or recurse into a folder
-   or vApp with depth protection
-   """
-   maxdepth = 10
+def _create_char_spinner():
+    """Creates a generator yielding a char based spinner.
+    """
+    while True:
+        for c in '|/-\\':
+            yield c
 
-   # if this is a group it will have children. if it does, recurse into them
-   # and then return
-   if hasattr(vm, 'childEntity'):
-      if depth > maxdepth:
-         return
-      vmList = vm.childEntity
-      for c in vmList:
-         PrintVmInfo(c, depth+1)
-      return
 
-   # if this is a vApp, it likely contains child VMs
-   # (vApps can nest vApps, but it is hardly a common usecase, so ignore that)
-   if isinstance(vm, vim.VirtualApp):
-      vmList = vm.vm
-      for c in vmList:
-         PrintVmInfo(c, depth + 1)
-      return
+_spinner = _create_char_spinner()
 
-   summary = vm.summary
-   print("Name       : ", summary.config.name)
-   print("Path       : ", summary.config.vmPathName)
-   print("Guest      : ", summary.config.guestFullName)
-   annotation = summary.config.annotation
-   if annotation != None and annotation != "":
-      print("Annotation : ", annotation)
-   print("State      : ", summary.runtime.powerState)
-   if summary.guest != None:
-      ip = summary.guest.ipAddress
-      if ip != None and ip != "":
-         print("IP         : ", ip)
-   if summary.runtime.question != None:
-      print("Question  : ", summary.runtime.question.text)
-   print("")
 
-def main():
-   """
-   Simple command-line program for listing the virtual machines on a system.
-   """
+def spinner(label=''):
+    """Prints label with a spinner.
 
-   args = GetArgs()
-   if args.password:
-      password = args.password
-   else:
-      password = getpass.getpass(prompt='Enter password for host %s and '
-                                        'user %s: ' % (args.host,args.user))
+    When called repeatedly from inside a loop this prints
+    a one line CLI spinner.
+    """
+    sys.stdout.write("\r\t%s %s" % (label, _spinner.next()))
+    sys.stdout.flush()
 
-   context = None
-   if hasattr(ssl, '_create_unverified_context'):
-      context = ssl._create_unverified_context()
-   si = SmartConnect(host=args.host,
-                     user=args.user,
-                     pwd=password,
-                     port=int(args.port),
-                     sslContext=context)
-   if not si:
-       print("Could not connect to the specified host using specified "
-             "username and password")
-       return -1
 
-   atexit.register(Disconnect, si)
+def get_obj(content, vim_type, name):
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vim_type, True)
+    for c in container.view:
+        if c.name == name:
+            obj = c
+            break
+    return obj
 
-   content = si.RetrieveContent()
-   for child in content.rootFolder.childEntity:
-      if hasattr(child, 'vmFolder'):
-         datacenter = child
-         vmFolder = datacenter.vmFolder
-         vmList = vmFolder.childEntity
-         for vm in vmList:
-            PrintVmInfo(vm)
-   return 0
 
-# Start program
-if __name__ == "__main__":
-   main()
+# form a connection...
+args = get_args()
+si = connect.SmartConnect(host=args.host, user=args.user, pwd=args.password,
+                          port=args.port)
+
+# doing this means you don't need to remember to disconnect your script/objects
+atexit.register(connect.Disconnect, si)
+
+content = si.RetrieveContent()
+
+esxi = get_obj(content,[vim.HostSystem], args.esxi)
+
+vms = esxi.vm
+
+for vm in vms:
+   print vm.name
+
+sys.exit(0)
+
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 VMware vSphere python api
 
 
-#### 010 VM operations
+#### 010 VM operations - finished.
 * start_vm
 * stop_vm
 * restart_vm
 * suspend_vm
 
-#### 020 VM web console
+#### 020 VM web console - finished.
 * vm_cdrom_iso
 * vm_web_console
 
@@ -21,7 +21,7 @@ VMware vSphere python api
 * mem
 * return result
 
-#### 040 Get VM status
+#### 040 Get VM status - finished.
 * started_vm
 * stopped_vm
 * suspended_vm
@@ -30,7 +30,7 @@ VMware vSphere python api
 #### 050 Get VM list from VMhost
 
 
-#### 060 Get VMhost from vm
+#### 060 Get VMhost from vm - finished.
 
 
 #### 070 Get VM matrics
